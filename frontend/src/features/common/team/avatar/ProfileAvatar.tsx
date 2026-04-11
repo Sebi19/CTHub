aefbdf759bf4cdbd422342d2efdd6f3317adaff3
Sebi19 <s.schreitter@gmx.at>
2026-04-11 18:13:34 +0200

    Fix search width and tooltips and adjust search team avatars to make it consistent

--- frontend/src/features/common/team/avatar/ProfileAvatar.tsx
+++ frontend/src/features/common/team/avatar/ProfileAvatar.tsx
@@ -5,17 +5,20 @@ import {IconUsers} from "@tabler/icons-react";
 interface ProfileAvatarProps extends Omit<AvatarProps, 'children'> {
     avatarUrl?: string;
     hideNoImage?: boolean;
+    profileAvailable?: boolean;
 }
 
-export const ProfileAvatar = ({ avatarUrl, size = "md", hideNoImage = false, ...others }: ProfileAvatarProps) => {
+export const ProfileAvatar = ({ avatarUrl, size = "md", hideNoImage = false, profileAvailable = true, ...others }: ProfileAvatarProps) => {
     const { t } = useTranslation();
 
     if (!avatarUrl && hideNoImage) {
         return null;
     }
 
+    const label = profileAvailable ? t("app.common.team.avatar.noImage") : t("app.common.team.avatar.noProfile");
+
     return (
-        <Tooltip label={t("app.common.team.avatar.noImage")} disabled={!!avatarUrl}>
+        <Tooltip label={label} disabled={!!avatarUrl} zIndex={2000}>
             <Avatar src={avatarUrl} radius="50%" size={size} color="blue" variant="light"  {...others}>
                 <IconUsers size="60%" />
             </Avatar>
--- frontend/src/features/common/team/avatar/SeasonTeamAvatar.tsx
+++ frontend/src/features/common/team/avatar/SeasonTeamAvatar.tsx
@@ -2,12 +2,13 @@ import {Avatar, type AvatarProps, Tooltip} from "@mantine/core";
 import {IconUsers} from "@tabler/icons-react";
 import {useTranslation} from "react-i18next";
 import {ProfileAvatar} from "./ProfileAvatar.tsx";
-import type {SeasonTeamDto} from "../../../../api/generated.ts";
+import type {SeasonTeamSearchResultDto} from "../../../../api/generated.ts";
 
 interface SeasonTeamAvatarProps extends Omit<AvatarProps, 'children'> {
-    team: SeasonTeamDto;
+    team: SeasonTeamSearchResultDto;
     hideNoProfile?: boolean;
     hideNoImage?: boolean;
+    showBlue?: boolean;
 }
 
 export const SeasonTeamAvatar = ({
@@ -15,17 +16,18 @@ export const SeasonTeamAvatar = ({
     size = "md",
     hideNoProfile = false,
     hideNoImage = false,
+    showBlue = false,
     ...others
 }: SeasonTeamAvatarProps) => {
     const { t } = useTranslation();
 
     const profile = team.seasonTeamProfile;
 
-    if (!profile) {
+    if (!profile && !showBlue) {
         if (hideNoProfile || hideNoImage) return null;
 
         return (
-            <Tooltip label={t("app.common.team.avatar.noProfile")}>
+            <Tooltip label={t("app.common.team.avatar.noProfile")} zIndex={2000}>
                 <Avatar radius="50%" size={size} color="gray" variant="transparent" opacity={0.3} {...others}>
                     <IconUsers size="60%" />
                 </Avatar>
@@ -34,6 +36,6 @@ export const SeasonTeamAvatar = ({
     }
 
     return (
-        <ProfileAvatar avatarUrl={profile.seasonAvatarUrl} size={size} hideNoImage={hideNoImage} {...others} />
+        <ProfileAvatar avatarUrl={profile?.seasonAvatarUrl} size={size} hideNoImage={hideNoImage} profileAvailable={!!profile} {...others} />
     );
 }

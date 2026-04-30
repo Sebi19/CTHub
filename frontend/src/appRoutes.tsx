5c374e0bd7a97fae4f84393c5d7d52e9d2fb28b8
Sebi19 <s.schreitter@gmx.at>
2026-04-30 16:12:55 +0200

    Update index route to redirect to dach final

--- frontend/src/appRoutes.tsx
+++ frontend/src/appRoutes.tsx
@@ -18,8 +18,8 @@ export function AppRoutes() {
                         */}
             <Route path="/" element={<App/>}>
 
-                {/* Index: Redirect to rootbots */}
-                <Route index element={<Navigate to="/rootbots/2025-26" replace/>}/>
+                {/* Index: Redirect to dach final */}
+                <Route index element={<Navigate to="/competition/2025-26/finale-2025-26" replace/>}/>
 
                 <Route path="/login" element={!isAuthenticated ? <LoginPage/> : <Navigate to="/"/>}/>
 
